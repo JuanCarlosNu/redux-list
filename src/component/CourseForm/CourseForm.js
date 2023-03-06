130d3757f3dd03f9c44ba323914ed564b12e1d0a
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-03-06 15:22:46 -0300

    inputSearch + event.target.value

--- src/component/CourseForm/CourseForm.js
+++ src/component/CourseForm/CourseForm.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './CourseForm.css';
+import { Connect } from 'react-redux';
+import {searchCourses} from '../../actions/actions'
 
 
-const CourseForm = () => {
+const CourseForm = ({searchCourses}) => {
     return (  
         <form className='search__Form'>
-            <input type='text' placeholder='search for a course..' className='search__input'/>
+            <input
+            type='text'
+            placeholder='search for a course..' 
+            className='search__input' 
+              onChange={(event)=>searchCourses(event.target.value)}/>
        </form>
     );
 }
  
-export default CourseForm
+export default Connect(null  , {searchCourses})(CourseForm);    
