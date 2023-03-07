67afdc362d6a9b672ac1c1b34ec3285fa4ca78bf
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-03-07 16:44:13 -0300

    style the navBar

--- src/component/NavBar/NavBar.js
+++ src/component/NavBar/NavBar.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import '../NavBar/NavBar.css'
 
 const NavBar = () => {
     return (  
    <div>
     <nav className='Header__nav'>
         <ul className='header__list'>
-          <li className='header__item'><Link to='/home'>Home</Link></li>
-          <li className='header__item'><Link to='/about'>About</Link></li>
-          <li className='header__item'><Link to='/blog'>Blog</Link></li>
-          <li className='header__item'><Link to='/saved'>Saved</Link></li>
+          <li className='header__item'><Link className='header__link' to='/home'>Home</Link></li>
+          <li className='header__item'><Link className='header__link' to='/about'>About</Link></li>
+          <li className='header__item'><Link className='header__link' to='/blog'>Blog</Link></li>
+          <li className='header__item'><Link className='header__link' to='/saved'>Saved</Link></li>
         </ul>
     </nav>
    
