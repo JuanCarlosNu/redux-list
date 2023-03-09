4d04600896f51702d053942a0836e6ea8adfb76e
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-03-09 17:06:03 -0300

    course detail- create file and link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import CoursesList from './component/coursesList/CoursesList';
 import { Route, Switch, Redirect } from 'react-router-dom';
-
 import SavedList from './component/savedList/savedList';
 import NavBar from './component/NavBar/NavBar';
+import CourseDetails from './component/CourseDetails/CourseDetails';
 
 
 
@@ -14,6 +14,7 @@ const App= ()=> {
       <Switch>
       <Route path="/home" exact component={CoursesList} />
       <Route path="/saved" exact component={SavedList}/>
+      <Route path="/coursedetail" exact component={CourseDetails} />
       <Redirect from='/' to='home' />
       <Redirect  to='home' />
       </Switch>
